c378a93e6b6c38783ca95b476d6da86b61aeddc3
MomentTum <manapong.bun@gmail.com>
2023-10-28 10:12:29 +0700

    abdomen finish

--- src/Components/ButtonSelection.jsx
+++ src/Components/ButtonSelection.jsx
@@ -2,7 +2,10 @@ export default function ButtonSelection(props) {
   const { w, top, left, onClick } = props;
   return (
     <>
-      <button onClick={onClick} className={`${w} h-11 absolute rounded-full ${top} ${left}`}></button>
+      <button
+        onClick={onClick}
+        className={`${w} h-11 absolute rounded-full ${top} ${left}`}
+      />
     </>
   );
 }
---
+++ src/Components/ImgDefaultAbs.jsx
@@ -0,0 +1,3 @@
+export default function ImgDefaultAbs() {
+  return <img src='abdomen/default-abs.png' alt='abd' className='w-full h-full object-contain' />;
+}
